126d94da6c22258badbdb7efa2cb35621e877820
paulo-oliveira021 <pauloaaoliveira@outlook.com>
2024-06-04 17:16:13 -0300

    coloquei a barra de pesquisa em portugues

--- search_bar.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var searchBox = document.querySelector('.nav-search input[type="search"]');
-  if (searchBox) {
-    searchBox.setAttribute('placeholder', 'Buscar...');
-  }
-});
